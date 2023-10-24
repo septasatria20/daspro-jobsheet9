805fb00f17805e113700f1a2b740a4d0a59c5d1a
Dwi Septa Satria Agung <144086401+septasatria20@users.noreply.github.com>
2023-10-24 13:34:10 +0700

    Add files via upload

---
+++ ArrayNilai10_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ArrayNilai10_1 {
+    public static void main(String[] args) {
+        Scanner sc10 = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            nilaiAkhir[i] = sc10.nextInt();
+        }
+            for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+            }
+    }
+}
