0b5ebc8941c9e6b19a09904d31d5964c7895be13
Dwi Septa Satria Agung <144086401+septasatria20@users.noreply.github.com>
2023-10-24 13:33:21 +0700

    Add files via upload

---
+++ ArrayNilai10_3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArrayNilai10_3 {
+    public static void main(String[] args) {
+        Scanner sc10 = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            nilaiAkhir[i] = sc10.nextInt();
+        }
+            for (int i = 0; i < nilaiAkhir.length; i++) {
+                if (nilaiAkhir[i] > 70) {
+                    System.out.println("Mahasiswa ke-" + i + " lulus!");
+                } else {
+                    System.out.println("Mahasiswa ke-" + i + " Tidak lulus!");
+                }
+            }
+    }
+}
