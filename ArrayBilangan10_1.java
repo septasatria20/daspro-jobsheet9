d33b1f3e0e76d125f661cbf3a0f9f6251ed8ced0
Dwi Septa Satria Agung <144086401+septasatria20@users.noreply.github.com>
2023-10-24 12:38:04 +0700

    Add files via upload

---
+++ ArrayBilangan10_1.java
@@ -0,0 +1,13 @@
+public class ArrayBilangan10_1 {
+    public static void main(String[] args) {
+        int [] bil = new int [4];
+        bil [0] = 5;
+        bil [1] = 13;
+        bil [2] = -7;
+        bil [3] = 17;
+        
+        for (int i = 0; i < 4; i++) {
+        System.out.println(bil[i]);
+        }
+    }
+}
